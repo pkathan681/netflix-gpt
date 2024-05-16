070af105524c3ecf01f09e219ff1563e68ad734a
Kathan Patel <pkathan681@gmail.com>
2024-05-16 18:29:58 +0530

    Multi Language Support in GPT Search Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,16 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {  useDispatch, useSelector } from 'react-redux';
 import { addUser , removeUser } from '../utils/userSlice';
-import { LOGO } from '../utils/constants';
+import { LOGO, SUPPORTED_LANGUAGES } from '../utils/constants';
 import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { toggleGptSearchView } from '../utils/gptSlice';
+import { changeLanguage } from '../utils/configSlice';
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector(store => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch) 
   const handleSignOut = () =>{
     signOut(auth)
     .then(()=>{
@@ -34,6 +37,13 @@ const Header = () => {
   // Unsubscribe when component unmounts
   return () => unsubscribe();
 },[])
+  const handleGptSearchClick = () => {
+    // Toggle GPT Search
+    dispatch(toggleGptSearchView());
+  };
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value))
+  };
   
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center flex justify-between">
@@ -43,6 +53,11 @@ const Header = () => {
           alt="logo"
         />
         {user &&(<div className="flex p-2">
+          { showGptSearch && (<select className="p-2 m-2 bg-gray-900 text-white" onChange={handleLanguageChange}>
+            {SUPPORTED_LANGUAGES.map((lang) => (<option key={lang.identifier} value={lang.identifier}>{lang.name}</option>))}
+
+          </select>)}
+          <button className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"onClick={handleGptSearchClick}>{showGptSearch? "HomePage":"GPT Search"}</button>
           <img className="w-12 h-12 " alt="usericon" src={user?.photoURL}/>
           <button onClick={handleSignOut} className="font-bold text-white" > (Sign Out) </button>
       </div>
